24d7bfab8a5be8eb70dcdf30f86e873e82b4b62b
kheyreddine <h.kheyreddine@gmail.com>
2023-10-07 22:50:30 +0200

    + feature: first draft of search functionality

--- src/app/nested-table/nested-table.component.ts
+++ src/app/nested-table/nested-table.component.ts
@@ -2,16 +2,22 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 import { TABLE_DATA } from 'src/assets/data';
 import { ResizeColumnDirective } from '../directives/resize-column.directive';
 import { Person } from '../models/person';
-
 @Component({
   selector: 'app-nested-table',
   templateUrl: './nested-table.component.html',
   styleUrls: ['./nested-table.component.scss'],
   standalone: true,
-  imports: [FormsModule, CommonModule, BrowserAnimationsModule, ResizeColumnDirective],
+  imports: [
+    FormsModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ResizeColumnDirective,
+  ],
 })
 export class NestedTableComponent {
   @Input() tableData: Person[] = TABLE_DATA;
@@ -24,6 +30,15 @@ export class NestedTableComponent {
 
   // Add a property to keep track of selected checkboxes
   anyCheckboxSelected: boolean = false;
+
+  private searchSubject = new Subject<string>();
+  private currentSearchQuery: string = ''; // Add this variable
+
+  constructor() {
+    this.searchSubject.pipe(debounceTime(300)).subscribe(() => {
+      this.filterTableData(this.currentSearchQuery); // Use the current search query
+    });
+  }
 
   // Add the rest of the functions here...
   selectAll(event: Event) {
@@ -51,8 +66,38 @@ export class NestedTableComponent {
     return this.selectedRows[row.name];
   }
 
-  onSearch(query: any) {
-    // Implement logic for searching and updating the displayed rows
+  // Function to filter the table data based on the search query
+  private filterTableData(query: string): void {
+    if (!query) {
+      this.tableData = TABLE_DATA; // Reset to original data when query is empty
+      return;
+    }
+
+    this.tableData = TABLE_DATA.filter(
+      (row) =>
+        row.name.toLowerCase().includes(query.toLowerCase()) ||
+        this.hasMatchingChild(row, query)
+    );
+  }
+
+  // Function to check if a row has a matching child
+  private hasMatchingChild(row: Person, query: string): boolean {
+    if (row.children) {
+      return row.children.some(
+        (child) =>
+          child.name.toLowerCase().includes(query.toLowerCase()) ||
+          this.hasMatchingChild(child, query)
+      );
+    }
+    return false;
+  }
+
+  // Function to handle search input
+  onSearch(event: any) {
+    const query = event.target.value;
+    this.currentSearchQuery = query; // Update the current search query
+
+    this.searchSubject.next(query);
   }
 
   deleteMultiple() {
